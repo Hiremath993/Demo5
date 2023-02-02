1c6d124247d10f69cb0a3f6679d1f0ed5a1c5d4f
Shweta Hiremath <shwetahiremath140@gmail.com>
2023-02-02 21:35:26 +0530

    first commit

---
+++ src/components/Post.jsx
@@ -0,0 +1,13 @@
+import classes from './Post.module.css';
+
+function Post({ author, body}){
+    return (
+    <>
+        <ul className={classes.post}>
+            <p className={classes.author}>{author}</p>
+            <p className={classes.text}>{body}</p>
+        </ul>
+    </>
+    );
+}
+export default Post;
